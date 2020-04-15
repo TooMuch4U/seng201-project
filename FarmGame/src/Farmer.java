6087655156616a9215efc17e22381d0f9bf7c7c0
Tom Rizzi <tcr59@uclive.ac.nz>
2020-04-15 15:26:03 +1200

    Implemented Farmer class

--- FarmGame/src/Farmer.java
+++ FarmGame/src/Farmer.java
@@ -1,9 +1,57 @@
 
 public class Farmer {
+	
+	private String name;
+	private int age;
+	
+	/**
+	 * Constructor for farmer.
+	 */
+	public Farmer() {
+		name = "Nameless Farmer";
+		age = 30;
+	}
+	
+	/**
+	 * Constructor with params.
+	 * 
+	 * @param newName Name the farmer will be initialised with.
+	 * @param newAge Age the farmer will be initialised with.
+	 */
+	public Farmer(String newName, int newAge) {
+		this.setName(newName);
+		age = newAge;
+	}
+	
+	/**
+	 * Returns the farmers name.
+	 */
+	public String getName() {
+		return(name);
+	}
+	
+	/**
+	 * Sets the farmers name.
+	 * 
+	 * @param newName New name for the farmer. Must be at least 1 character.
+	 */
+	public void setName(String newName) {
+		if (newName.length() > 0) {
+			name = newName;
+		}
+		else {
+			throw new IllegalArgumentException("newName String must atleast be of length 1.");				
+		}
+	}
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
-		System.out.println("Test");
+		
+		Farmer farmer1 = new Farmer("Tom", 18);
+		Farmer farmer2 = new Farmer();
+		
+		
+		System.out.println(farmer1.getName());
+		System.out.println(farmer2.getName());
 	}
 
 }
