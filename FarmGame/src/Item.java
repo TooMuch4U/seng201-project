fc7c0de797a739771e2897b758a021108722b115
Tom Rizzi <tcr59@uclive.ac.nz>
2020-04-15 16:00:04 +1200

    Implemented Item class

---
+++ FarmGame/src/Item.java
@@ -0,0 +1,62 @@
+
+public class Item {
+	
+	/**
+	 * Items name
+	 */
+	private String name;
+	/**
+	 * Items price
+	 */
+	private double price;
+	/**
+	 * Items benefit
+	 */
+	private double benefit;
+	
+	
+	/**
+	 * Item constructor
+	 * 
+	 * @param newName
+	 * @param newPrice
+	 * @param newBenefit
+	 */
+	public Item(String newName, double newPrice, double newBenefit) {
+		name = newName;
+		price = newPrice;
+		benefit = newBenefit;
+	}
+	
+	/**
+	 * Returns the items name.
+	 * 
+	 * @return String
+	 */
+	public String getName() {
+		return(name);
+	}
+	
+	/**
+	 * Returns the items price.
+	 * 
+	 * @return
+	 */
+	public double getPrice() {
+		return(price);
+	}
+	
+	/**
+	 * Returns the items benefit.
+	 * 
+	 * @return double
+	 */
+	public double getBenefit() {
+		return(benefit);
+	}
+	
+	public static void main(String[] args) {
+
+	}
+
+}
