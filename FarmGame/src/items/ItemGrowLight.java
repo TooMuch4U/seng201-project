a6b8e8d7f29e430981af6ef5def8221aa5e172b0
Tom Rizzi <tcr59@uclive.ac.nz>
2020-04-21 17:12:20 +1200

    Renamed GrowLight class

--- FarmGame/src/items/ItemGrowLight.java
+++ FarmGame/src/items/ItemGrowLight.java
@@ -1,11 +1,11 @@
 package items;
 
-public class ItemGrowLights extends ItemForCrop {
+public class ItemGrowLight extends ItemForCrop {
 	
 	/**
 	 * Constructs the item with correct values
 	 */
-	public ItemGrowLights() {
+	public ItemGrowLight() {
 		super("Hydroponic Grow Lights", 24.99, 3.0);
 	}
 
