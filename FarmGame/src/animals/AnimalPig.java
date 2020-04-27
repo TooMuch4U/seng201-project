ece69e3c0d36f551e986fc8ce870e06941ceadd6
Dan Chapman <danjpchapman14@gmail.com>
2020-04-27 16:21:06 +1200

    "Added animal and crop packages"

---
+++ FarmGame/src/animals/AnimalPig.java
@@ -0,0 +1,12 @@
+package animals;
+
+
+public class AnimalPig extends Animal {
+	
+	/**
+	 * Constructor for the pig type of animal
+	 */
+	public AnimalPig() {
+		super("Pig", 250.00, 100.00, 70.00, 80.00);
+	}
+}
---
+++ FarmGame/src/crops/CropCabbage.java
@@ -0,0 +1,12 @@
+package crops;
+
+
+public class CropCabbage extends Crop {
+	
+	/**
+	 * Constructor for the cabbage type of crop
+	 */
+	public CropCabbage() {
+		super("Cabbage", 10.00, 30.00, 2);
+	}
+}
