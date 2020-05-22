5707aa3afca8ce39a46db1e54746b1bfb5596ca8
Dan Chapman <danjpchapman14@gmail.com>
2020-05-22 17:19:32 +1200

    Checked over docstrings and test cases

--- FarmGame/src/farmsAndFarmer/FarmBasic.java
+++ FarmGame/src/farmsAndFarmer/FarmBasic.java
@@ -1,14 +1,14 @@
 package farmsAndFarmer;
 
 /**
- * Farm type that grants no additional bonuses
+ * Farm type that grants no additional bonuses.
  */
 public class FarmBasic extends Farm {
 	
 	/**
-	 * Constructor for the farm with parameters
-	 * @param name - the name of the farm
-	 * @param farmer - the farmer created by the user
+	 * Constructor for the farm with parameters.
+	 * @param name - the name of the farm.
+	 * @param farmer - the farmer created by the user.
 	 */
 	public FarmBasic(String name, Farmer farmer) {
 		super(name, "basic", farmer, 1000.00);
